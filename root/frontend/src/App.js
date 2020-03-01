4a063109afc3ca125a3a136a26aa076c5ed2571f
Kevin Soruco <ksoruco@strydenyc.com>
2020-03-01 10:53:00 +0800

    Add eslint, basic app structure

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Button from './components/Button';
+import Todos from './containers/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: ''
+    }
+  }
+
+  handleOnChange = (e) => {
+    this.setState({
+      todo: e.target.value
+    });
+    console.log(this.state)
+  };
+
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.setState({
+      todo: '',
+    });
+  }
+  
+  render() {
+    const { todo } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Todos App</h1>
+        </header>
+          <form onSubmit={this.handleOnSubmit}>
+            <input
+              type="text" 
+              name="todo"
+              onChange={this.handleOnChange}
+              value={todo}
+            />
+            <Button 
+              type="submit" 
+              text="Add"
+              disabled={!todo}
+            />
+          </form>
+          <div className="todos-container">
+            <Todos />
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
